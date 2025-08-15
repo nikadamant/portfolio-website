30e43acc80c907b3b317003484edba924aa494bf
Nikita Borysov <nborysov15@gmail.com>
2025-08-15 07:45:38 +0300

    Projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      title: 'Fit Coach',
+      image: '/images/fit-coach.png',
+      alt: 'Fit coach project',
+      github: 'https://github.com/nikadamant/fit-coach'
+    },
+    {
+      id: 2,
+      title: 'Portfolio',
+      image: '/images/portfolio.png',
+      alt: 'Portfolio project',
+      github: 'https://github.com/nikadamant/html-portfolio-css-in-html-'
+    },
+    {
+      id: 3,
+      title: 'Bose landing page',
+      image: '/images/bose-landing.png',
+      alt: 'Bose landing page project',
+      github: 'https://github.com/nikadamant/Landing'
+    },
+    {
+      id: 4,
+      title: 'Flexbox pricing table',
+      image: '/images/flexbox-pricing-table.png',
+      alt: 'Flexbox pricing table project',
+      github: 'https://github.com/nikadamant/flexbox-pricing-table-project'
+    }
+  ];
+
+  return (
+    <section id="projects" className="projects">
+      <div className="projects-container">
+        <h2>My Projects</h2>
+        <div className="project-grid">
+          {projects.map((project) => (
+            <div key={project.id} className="project">
+              <img
+                src={project.image}
+                alt={project.alt}
+                className="project-image"
+              />
+              <h3 className="project-title">{project.title}</h3>
+              <a
+                className="project-github"
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects; 
