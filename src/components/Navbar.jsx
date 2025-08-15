30b615073f4f0996ef4c6d1193ad882758047397
Nikita Borysov <nborysov15@gmail.com>
2025-08-15 07:45:01 +0300

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navbar = () => {
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <button className="nav-link" onClick={() => scrollToSection('about')}>
+            About Me
+          </button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link" onClick={() => scrollToSection('resume')}>
+            Resume
+          </button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link" onClick={() => scrollToSection('projects')}>
+            Projects
+          </button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link" onClick={() => scrollToSection('contact')}>
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar; 
