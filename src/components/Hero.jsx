4e7e447957f320f4a501e3ddb250d215099ec07f
Nikita Borysov <nborysov15@gmail.com>
2025-08-15 07:33:06 +0300

    Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1 className="hero-title">Hi! My name is Nikita</h1>
+      <p className="hero-subtitle">This is my website</p>
+    </section>
+  );
+};
+
+export default Hero; 
