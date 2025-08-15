9d67914e4d86adbaa184d7ed810c242105c85593
Nikita Borysov <nborysov15@gmail.com>
2025-08-15 07:37:57 +0300

    AboutMe component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,23 @@
+
+const AboutMe = () => {
+  return (
+    <section id="about" className="about-me">
+      <div className="about-container">
+        <h2>About Me</h2>
+        <p>
+          I am a passionate web developer with experience in various
+          technologies.
+        </p>
+        <div className="about-images">
+          <img
+            src="/images/profile.png"
+            alt="Profile Image 1"
+            className="about-image"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe; 
