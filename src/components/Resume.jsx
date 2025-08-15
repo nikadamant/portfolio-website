ec569288cec87c47d50332cfb7468337664c83fa
Nikita Borysov <nborysov15@gmail.com>
2025-08-15 10:58:13 +0300

    Styles improded and icons added for skills

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,4 +1,24 @@
 
+import React from 'react';
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiPython,
+  SiReact,
+  SiNodedotjs,
+  SiExpress,
+  SiDotnet,
+  SiPostgresql,
+  SiMongodb,
+  SiRedis,
+  SiGit,
+  SiGithub,
+  SiPostman
+} from 'react-icons/si';
+import { TbBrandCSharp, TbBrandVscode } from 'react-icons/tb';
+import { DiMsqlServer, DiVisualstudio } from 'react-icons/di';
+
 const Resume = () => {
   return (
     <section id="resume" className="resume">
@@ -7,47 +27,100 @@ const Resume = () => {
         <div className="resume-content">
           <div className="skills">
             <h3 className="skills-title">Programming Languages</h3>
-            <ul>
-              <li>HTML, CSS, JavaScript</li>
-              <li>ES6</li>
-              <li>C#</li>
-              <li>Python</li>
-              <li>SQL</li>
-              <li>NoSQL</li>
+            <ul className="skills-list">
+              <li className="skill-item">
+                <SiHtml5 className="skill-icon" />
+                <span>HTML</span>
+              </li>
+              <li className="skill-item">
+                <SiCss3 className="skill-icon" />
+                <span>CSS</span>
+              </li>
+              <li className="skill-item">
+                <SiJavascript className="skill-icon" />
+                <span>JavaScript</span>
+              </li>
+              <li className="skill-item">
+                <TbBrandCSharp className="skill-icon" />
+                <span>C#</span>
+              </li>
+              <li className="skill-item">
+                <SiPython className="skill-icon" />
+                <span>Python</span>
+              </li>
             </ul>
           </div>
 
           <div className="skills">
             <h3 className="skills-title">Frameworks/Libraries</h3>
-            <ul>
-              <li>React</li>
-              <li>Node.js</li>
-              <li>Express</li>
-              <li>EJS</li>
-              <li>.NET</li>
-              <li>WF</li>
+            <ul className="skills-list">
+              <li className="skill-item">
+                <SiReact className="skill-icon" />
+                <span>React</span>
+              </li>
+              <li className="skill-item">
+                <SiNodedotjs className="skill-icon" />
+                <span>Node.js</span>
+              </li>
+              <li className="skill-item">
+                <SiExpress className="skill-icon" />
+                <span>Express</span>
+              </li>
+              <li className="skill-item">
+                <SiJavascript className="skill-icon" />
+                <span>EJS</span>
+              </li>
+              <li className="skill-item">
+                <SiDotnet className="skill-icon" />
+                <span>.NET</span>
+              </li>
             </ul>
           </div>
 
           <div className="skills">
             <h3 className="skills-title">Databases</h3>
-            <ul>
-              <li>SQL Server</li>
-              <li>PostgreSQL(basic)</li>
-              <li>MongoDB(basic)</li>
-              <li>Redis(basic)</li>
+            <ul className="skills-list">
+              <li className="skill-item">
+                <DiMsqlServer className="skill-icon" />
+                <span>SQL Server</span>
+              </li>
+              <li className="skill-item">
+                <SiPostgresql className="skill-icon" />
+                <span>PostgreSQL(basic)</span>
+              </li>
+              <li className="skill-item">
+                <SiMongodb className="skill-icon" />
+                <span>MongoDB(basic)</span>
+              </li>
+              <li className="skill-item">
+                <SiRedis className="skill-icon" />
+                <span>Redis(basic)</span>
+              </li>
             </ul>
           </div>
 
           <div className="skills">
             <h3 className="skills-title">Tools</h3>
-            <ul>
-              <li>Visual Studio</li>
-              <li>Visual Studio Code</li>
-              <li>Git</li>
-              <li>GitHub</li>
-              <li>SQL Server Management Studio</li>
-              <li>Postman(basic)</li>
+            <ul className="skills-list">
+              <li className="skill-item">
+                <SiGit className="skill-icon" />
+                <span>Git</span>
+              </li>
+              <li className="skill-item">
+                <SiGithub className="skill-icon" />
+                <span>GitHub</span>
+              </li>              <li className="skill-item">
+                <DiVisualstudio className="skill-icon" />
+                <span>Visual Studio</span>
+              </li>
+              <li className="skill-item">
+                <TbBrandVscode className="skill-icon" />
+                <span>Visual Studio Code</span>
+              </li>
+              <li className="skill-item">
+                <SiPostman className="skill-icon" />
+                <span>Postman(basic)</span>
+              </li>
             </ul>
           </div>
         </div>
